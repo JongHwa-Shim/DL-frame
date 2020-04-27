56d5d848bb0a786392e0b2986339948ae66dd5b3
JongHwa-Shim <shimjonghwa@kakao.com>
2020-04-27 18:20:32 +0900

    little fix message.leave_log >> ValidLoss -> Valid_Loss

--- message.py
+++ message.py
@@ -5,7 +5,7 @@ def leave_log (losses, accuracy_list, epoch, mode):
     if mode=='train':
         base = ("Epoch: {epoch:d}  Train_Loss: {loss:.8}  Train_Accuracy: {accuracy:.8}")
     else:
-        base = ("Epoch: {epoch:d}  ValidLoss: {loss:.8}  Valid_Accuracy: {accuracy:.8}")
+        base = ("Epoch: {epoch:d}  Valid_Loss: {loss:.8}  Valid_Accuracy: {accuracy:.8}")
 
     message = base.format(epoch=epoch, loss=loss, accuracy=accuracy)
     return message, loss, accuracy
